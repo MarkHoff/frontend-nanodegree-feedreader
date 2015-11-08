dc093470bcc9eb2e973265e8819094c742573888
Mark Hoffman <mh4913@att.com>
2015-11-08 16:41:18 -0500

    Updated comments in feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,25 +4,15 @@
  * all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
+	
+    /* The RSS Feeds test suite tests the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
+   
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-                
-        /* TODO: Write a test that loops through each feed
+                        
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -34,7 +24,7 @@ $(function() {
             }
          });
          
-        /* TODO: Write a test that loops through each feed
+        /* This test loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -48,7 +38,8 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* The menu test suite tests the menu functions */
+   
 	describe('The menu', function() {
         var menu;
 
@@ -56,20 +47,16 @@ $(function() {
             menu = $('.menu-icon-link');
         });
 			
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+        /* This test ensures the menu element is
+         * hidden by default.
          */
         
 		it('is hidden by default', function() {
             expect($("body").hasClass("menu-hidden")).toBe(true);
 			});
 			
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
+         /* This test ensures the menu changes
+          * visibility when the menu icon is clicked. 
           */
          
          it('is visible when clicked, and disappears when clicked a second time', function() {
@@ -87,16 +74,14 @@ $(function() {
         });
 	});
 	
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Test suite "Initial Entries" */
    
    describe('Initial Entries', function() {
    	var entry;
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This test ensures that when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test wil require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
         
         /* Simulate the asynchronous function */
@@ -114,15 +99,13 @@ $(function() {
    	
    });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Test suite "New Feed Selection" */
 
 	describe('New Feed Selection', function() {
 		var entry;
 
-	
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+        /* This test ensures that when a new feed is loaded
+         * by the loadFeed function the content actually changes.
          */
         
         /* Simulate the asynchronous function */
